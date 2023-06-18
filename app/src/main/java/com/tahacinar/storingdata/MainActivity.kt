0c02ab7b1f5ff2c31c5b4ea1f054219256eb945c
Muhammed Taha Çınar <tahacinar@outlook.com.tr>
2023-06-18 20:51:18 +0300

    v.1.0.

--- app/src/main/java/com/tahacinar/storingdata/MainActivity.kt
+++ app/src/main/java/com/tahacinar/storingdata/MainActivity.kt
@@ -1,11 +1,34 @@
 package com.tahacinar.storingdata
 
+import android.content.Context
+import android.content.SharedPreferences
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import com.tahacinar.storingdata.databinding.ActivityMainBinding
+import java.util.prefs.Preferences
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
+    private lateinit var sharedPref : SharedPreferences
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        val view = binding.root
+        setContentView(view)
+
+        sharedPref = this.getSharedPreferences("com.tahacinar.storingdata", Context.MODE_PRIVATE)
+
     }
+
+    fun save (view:View){
+
+    }
+    fun delete (view:View){
+
+    }
+
+
 }
